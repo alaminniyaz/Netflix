4e3c0f1fd8c271b70aef7dbf53e51e977c6d26b3
Al Amin <Mdalamin4064@gmail.com>
2023-01-31 19:16:35 +0600

    i'm single, marry me

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-let languageDropdown = document.getElementById("language-btn")
 let faqListItem = document.querySelectorAll(".inner-container ul li")
-
-languageDropdown.addEventListener("click", () => {
-  document.getElementById("language-dropdown").classList.toggle("show")
-})
 
 faqListItem.forEach((item) => {
   item.addEventListener("click", (e) => {
